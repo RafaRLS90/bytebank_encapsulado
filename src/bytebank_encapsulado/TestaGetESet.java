d2fe40ab7ce950f9486604d8ad7f3786363e27c0
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-20 18:30:39 -0300

    aprendendo a usar referencias

--- src/bytebank_encapsulado/TestaGetESet.java
+++ src/bytebank_encapsulado/TestaGetESet.java
@@ -6,5 +6,18 @@ public class TestaGetESet {
 		//conta.numero = 1337;
 		conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente rafael = new Cliente();
+		//conta.titular = rafael;
+		rafael.setNome("rafael lima");
+		
+		conta.setTitular(rafael);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		// agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("programador");
 	}
 }
